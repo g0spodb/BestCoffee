56444c8eb724c666e13458e08812782bb6b6cb78
Николаев Клементий <klim_nikolaev03@mail.ru>
2022-05-29 20:54:37 +0300

    Добавьте файлы проекта.

---
+++ WPFBestCoffee/RecipesPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFBestCoffee
+{
+    /// <summary>
+    /// Логика взаимодействия для RecipesPage.xaml
+    /// </summary>
+    public partial class RecipesPage : Page
+    {
+        public RecipesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRecipes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BtnFavorite_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BtnHistory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
